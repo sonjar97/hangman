eab4b1c47bd2418e8854a57f72bbb38cee92f3ce
Sondre Jaros <62839873+sonjar97@users.noreply.github.com>
2023-01-16 16:08:19 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -10,4 +10,4 @@ guess = input('Please enter a single letter: ')
 if len(guess) == 1 and guess.lower() >= 'a' and guess.lower() <= 'z':
   print('Good guess!')
 else:
-  print('Oops? That\'s not a letter')
+  print('Oops! That is not a valid input.')
