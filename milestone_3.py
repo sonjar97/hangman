4a24aac729f72191c838696dced970d63d4a1326
Sondre Jaros <62839873+sonjar97@users.noreply.github.com>
2023-01-16 16:39:53 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,37 +1,53 @@
 import random
 
 
+def check_guess(guess):
+  guess = guess.lower()
+  
+  if guess in word:
+    print('Good guess! {} is in the word.'.format(guess))
+  else:
+    print('Sorry, {} is not in the word. Try again.'.format(guess))
+    
+    
+def ask_for_input():
+  while True:
+    guess = input('Please enter a single letter: ')
+    if len(guess) == 1 and a.isalpha():
+      break
+    else:
+      print('Invalid letter. Please, enter a single alphabetical character.')
+
+  check_guess(guess)
+  
+  
 word_list = ['Banana', 'Avocado', 'Apple', 'Grapefruit', 'Grapes']
 word = random.choice(word_list)
 print(word)
 
-while True:
-  guess = input('Please enter a single letter: ')
-  if len(guess) == 1 and a.isalpha():
-    break
-  else:
-    print('Invalid letter. Please, enter a single alphabetical character.')
-
-if guess in word:
-  print('Good guess! {} is in the word.'.format(guess))
-else:
-  print('Sorry, {} is not in the word. Try again.'.format(guess))
-
-
-
-
-
-
+ask_for_input()
 
 
 /*
-You should use an if statement to check if the letter entered by the user is in the word. Remember that the input should be stored in a variable called "guess" and the randomly picked word should be stored in a variable called "word". You can use the "in" operator to check if a character is in a string. If you think you have done it correctly, remember to use the right indentation for the if statement and just a single space between the "if" keyword and the condition
+Good job so far! But your code probably doesn't look great. It's hard to tell which lines do what.
 
-Check whether the letter guessed by the user is in the secret word that was randomly chosen by the computer. For example, if the user guesses the letter "a" and the secret word is "apple", then your code should check if "a" is in "apple".
+Create 2 functions, check_guess and ask_for_input functions which contain the code for those two things.
 
-Step 1. Create an if statement that checks if the guess is in the word.
+The check_guess function will take the guessed letter as an argument and check if the letter is in the word.
 
-Step 2. In the body of the if statement, print a message saying "Good guess! {guess} is in the word.". Obviously, format the string to show the actual guess instead of {guess}.
+Step 1: Define a function called check_guess. pass in the guess as a parameter for the function. Write the code for the following steps in the body of this function.
 
-Step 3. Create an else block that prints a message saying "Sorry, {guess} is not in the word. Try again." This block of code will run if the guess is not in the word.
+Step 2: Convert the guess into lower case.
+
+Step 3. Move the code that you wrote to check if the guess is in the word into this function block.
+
+The ask_for_input function.
+
+Step 1. Define a function called ask_for_input.
+
+Step 2. Move the code that you wrote in the Iteratively check if the input is a valid guess task into this function block.
+
+Step 3. Outside the while loop, but within this function, call the check_guess function to check if the guess is in the word. Don't forget to pass in the guess as an argument to the method.
+
+Step 4. Outside the function, call the ask_for_input function to test your code.
 */
