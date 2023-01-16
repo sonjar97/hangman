1dbe89662131d22c3e962ab1e16849b65bf3111b
Sondre Jaros <62839873+sonjar97@users.noreply.github.com>
2023-01-16 17:21:03 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -5,8 +5,8 @@ class Hangman:
   def __init__(self, word_list, num_lives=5):
     self.word_list = word_list
     self.num_lives = num_lives
-    self.word = word = random.choice(word_list).lower()
+    self.word = word = random.choice(word_list)
     
     self.word_guessed = ['_'] * len(word)
-    self.num_letters = len(set(word))
+    self.num_letters = len(set(word.lower()))
     self.list_of_guesses = []
