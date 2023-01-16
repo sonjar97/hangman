6daaa4dec657097f3fcbc22e86363feacb7e0b7e
Sondre Jaros <62839873+sonjar97@users.noreply.github.com>
2023-01-16 16:26:04 +0000

    Create milestone_3.py

---
+++ milestone_3.py
@@ -0,0 +1,36 @@
+
+
+
+while True:
+  guess = input('Please enter a single letter: ')
+  if len(guess) == 1 and a.isalpha():
+    break
+  else:
+    print('Invalid letter. Please, enter a single alphabetical character.')
+
+
+
+
+
+
+
+
+
+
+/*
+Write code that will continuously ask the user for a letter and validate it.
+
+Create a new script called milestone_3.py. This file will contain the code for this milestone.
+
+Step 1. Create a while loop and set the condition to True. Setting the condition to True ensures that the code run continuously.
+
+In the body of the loop, write the code required for the following steps.
+
+Step 2: Ask the user to guess a letter and assign this to a variable called guess.
+
+Step 3. Check that the guess is a single, alphabetical character.
+
+Step 4. If the guess passes the checks, break out of the loop.
+
+Step 5: If the guess does not pass the checks, then print a message saying "Invalid letter. Please, enter a single alphabetical character."
+*/
