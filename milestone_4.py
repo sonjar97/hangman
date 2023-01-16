bb42786bb3ebc5d628dfd358c078df9edf8161ce
Sondre Jaros <62839873+sonjar97@users.noreply.github.com>
2023-01-16 18:33:00 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -20,7 +20,7 @@ class Hangman:
   
   def ask_for_input(self):
     i = 0
-    while i < 3:
+    while i < self.num_lives:
       guess = input('Please enter a single letter: ').lower()
       if not (len(guess) == 1 and guess.isalpha()):
         print('Invalid letter. Please, enter a single alphabetical character.')
@@ -28,4 +28,4 @@ class Hangman:
         print('You already tried that letter!')
       else:
         self.check_guess(guess)
-      i += 1
+        i += 1
